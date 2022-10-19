edad164ea6d2148fde6aeaca79057e3f67b8f7f3
Sharjeel Riaz <sharjeelriazsh@gmail.com>
2022-10-19 17:48:40 +0500

    Updated repo

---
+++ Fundamentals/First App Component/reactlab/src/App.js
@@ -0,0 +1,18 @@
+//This file won't work as its downloaded from the completed remote coursera lab server.
+//To make this work, you have to create a fresh pacakage using npm then make the necessary changes
+//to see the actual results.
+
+function Heading() {
+  return <h1>This is an h1 heading.</h1>;
+}
+
+function App() {
+  return (
+    <div className="App">
+      This is the starting code for "Your first component" ungraded lab
+      <Heading />
+    </div>
+  );
+}
+
+export default App;
