d81824cd4fe9428fe210980a8c05bc706800223c
Sharjeel Riaz <sharjeelriazsh@gmail.com>
2023-01-26 09:40:47 +0500

    Updated Sudoku

--- Projects/Sudoku/app.js
+++ Projects/Sudoku/app.js
@@ -10,18 +10,81 @@ document.querySelector("#checkbox").addEventListener("click", () => {
 });
 
 // Initial Values
-const name_input = document.querySelector("#input-name");
+
+// screens
 const start_screen = document.querySelector("#start-screen");
+const game_screen = document.querySelector("#game-screen");
+
+// -------
+const cells = document.querySelectorAll(".main-grid-cell");
+
+const name_input = document.querySelector("#input-name");
+
+const player_name = document.querySelector("#player-name");
+const game_level = document.querySelector("#game-level");
+const game_time = document.querySelector("#game-time");
 
 let level_index = 0;
 let level = CONSTANT.LEVEL[level_index];
 
+let timer = null;
+let pause = false;
+let seconds = 0;
 // --------
 
-// Button Events
+const getGameInfo = () => JSON.parse(localStorage.getItem("game"));
+
+// add space for each 9 cells
+const initGameGrid = () => {
+  let index = 0;
+
+  for (let i = 0; i < Math.pow(CONSTANT.GRID_SIZE, 2); i++) {
+    let row = Math.floor(i / CONSTANT.GRID_SIZE);
+    let col = i % CONSTANT.GRID_SIZE;
+    if (row === 2 || row === 5) cells[index].style.marginBottom = "10px";
+    if (col === 2 || col === 5) cells[index].style.marginRight = "10px";
+
+    index++;
+  }
+};
+// -------
+
+const setPlayerName = (name) => localStorage.setItem("player_name", name);
+const getPlayerName = () => localStorage.getItem("player_name");
+
+const showTime = (seconds) =>
+  new Date(seconds * 1000).toISOString().substr(11, 8);
+
+const startGame = () => {
+  start_screen.classList.remove("active");
+  game_screen.classList.add("active");
+
+  player_name.innerHTML = name_input.value.trim();
+  setPlayerName(name_input.value.trim());
+
+  game_level.innerHTML = CONSTANT.LEVEL_NAME[level_index];
+
+  seconds = 0;
+
+  timer = setInterval(() => {
+    if (!pause) {
+      seconds = seconds + 1;
+      game_time.innerHTML = showTime(seconds);
+    }
+  }, 1000);
+};
+
+// add Button Events
+document.querySelector("#btn-level").addEventListener("click", (e) => {
+  level_index =
+    level_index + 1 > CONSTANT.LEVEL.length - 1 ? 0 : level_index + 1;
+  level = CONSTANT.LEVEL[level_index];
+  e.target.innerHTML = CONSTANT.LEVEL_NAME[level_index];
+});
+
 document.querySelector("#btn-play").addEventListener("click", () => {
   if (name_input.value.trim().length > 0) {
-    alert(`level => ${level}`);
+    startGame();
   } else {
     name_input.classList.add("input-err");
     setTimeout(() => {
@@ -31,16 +94,7 @@ document.querySelector("#btn-play").addEventListener("click", () => {
   }
 });
 
-document.querySelector("#btn-level").addEventListener("click", (e) => {
-  level_index =
-    level_index + 1 > CONSTANT.LEVEL.length - 1 ? 0 : level_index + 1;
-  level = CONSTANT.LEVEL[level_index];
-  e.target.innerHTML = CONSTANT.LEVEL_NAME[level_index];
-});
-
 // -------
-
-const getGameInfo = () => JSON.parse(localStorage.getItem("game"));
 
 const init = () => {
   const darkmode = JSON.parse(localStorage.getItem("darkmode"));
@@ -55,5 +109,7 @@ const init = () => {
     ? "grid"
     : "none";
 };
+
+initGameGrid();
 
 init();
